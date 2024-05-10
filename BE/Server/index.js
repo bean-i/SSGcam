e1301fae51fe3ea66684063c2dcee6852800e391
isakacindy <isakacindy@gmail.com>
2024-05-11 02:21:28 +0900

    EDIT: Login Suceess

--- BE/Server/index.js
+++ BE/Server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://isakacindy:PNWADCm19pGj3bP6@server.2n9yl1k.mongodb.net/', {
@@ -9,6 +14,18 @@ mongoose.connect('mongodb+srv://isakacindy:PNWADCm19pGj3bP6@server.2n9yl1k.mongo
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(()=>{
+        res.status(200).json({
+            success:true
+        })
+    }).catch((err)=>{
+        return res.json({success:false, err})
+    })
 })
 
 app.listen(port, () => {
