543acc1b38543c2463e885057f1104f9c4ee20b6
isakacindy <isakacindy@gmail.com>
2024-05-11 02:05:30 +0900

    DELETE: NODE_MODULES

--- BE/Server/models/User.js
+++ BE/Server/models/User.js
@@ -3,26 +3,32 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength: 50
+        maxlength: 50,
+        required: true
     },
-    email:{
+    username: {
         type: String,
-        trim: true,
-        unique: 1
+        required: true,
+        unique: true
     },
     password: {
         type: String,
-        minlength: 5
+        minlength: 5,
+        required: true
     },
-    lastname: {
+    confirmPassword: {
         type: String,
-        maxlength: 50
+        minlength: 5,
+        required: true 
+    },
+    parentContact: {
+        type: String,
+        required: false 
     },
     role: {
         type: Number,
         default: 0
     },
-    image: String,
     token: {
         type: String
     },
