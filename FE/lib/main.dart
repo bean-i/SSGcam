45fc5dacbc55a6e7da87ee36c3b773614a0d18c1
yujin-zero <poly30@naver.com>
2024-05-17 15:17:20 +0900

    yujin FE 폴더

--- FE/lib/main.dart
+++ FE/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'screens/main_screen.dart';
 import 'package:logger/logger.dart';
+import 'screens/onboarding.dart';
+import 'screens/login.dart';
+import 'screens/signup.dart';
+import 'screens/permission.dart';
 
 var logger = Logger();
 
 void main() {
   logger.d("앱 시작");
+
   runApp(const MyApp());
 }
 
+// 초기 라우트 및 라우트 설정 정의
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -22,7 +27,10 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const MainScreen(),
+        '/': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/permissions': (context) => const PermissionScreen(),
       },
     );
   }
