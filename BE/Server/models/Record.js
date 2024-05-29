dc2e5fb32be66c3ca984d0674d6ede72191470cf
isakacindy <isakacindy@gmail.com>
2024-05-29 09:59:55 +0900

    위험레벨 범주 수정

--- BE/Server/models/Record.js
+++ BE/Server/models/Record.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// const ObjectId = mongoose.Schema.Types.ObjectId; 
 
 const recordSchema = new mongoose.Schema({
     rc_user_id: {
@@ -14,22 +13,35 @@ const recordSchema = new mongoose.Schema({
     rc_fd_category: {
         type: String,
         required: true,
-        enum: ['수사기관사칭형', '대출사기형', '기타']
+        enum: ['기관 사칭형 보이스피싱', '대출 사기형 보이스피싱', '기타']
     },
     rc_fd_level: {
-        type: String,
-        required: true,
-        enum: ['낮음', '보통', '높음'] //높음, 위험
+        type: Number, 
+        required: true
     },
     rc_audio_file: {
         type: String,
-        //type: mongoose.Schema.Types.ObjectId,
         required: true
     },
     createdAt: {
         type: Date,
-        default: Date .now
+        default: Date.now
     }
 });
+
+recordSchema.virtual('rc_fd_level_str').get(function() {
+    if (this.rc_fd_level >= 80) {
+        return '위험';
+    } else if (this.rc_fd_level >= 60) {
+        return '경고';
+    } else if (this.rc_fd_level >= 40) {
+        return '보통';
+    } else {
+        return '낮음';
+    }
+});
+
+recordSchema.set('toJSON', { virtuals: true });
+recordSchema.set('toObject', { virtuals: true });
 
 module.exports = mongoose.model('Record', recordSchema);
--- BE/Server/service/recordService.js
+++ BE/Server/service/recordService.js
@@ -12,12 +12,24 @@ async function findByPhoneNumber(phoneNumber) {
 async function createRecord(data) {
   const record = new Record(data);
   try {
-    await record.save();
-    return record;
+    const savedRecord = await record.save();
+    console.log(savedRecord);
+    console.log('위험 레벨:', savedRecord.rc_fd_level_str); 
+    return savedRecord;
   } catch (error) {
     throw error;
   }
 }
+
+// async function createRecord(data) {
+//   const record = new Record(data);
+//   try {
+//     await record.save();
+//     return record;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
 module.exports = {
   findByPhoneNumber,
