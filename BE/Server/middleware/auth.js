b34762f664a856726dba057351f041852350fca7
isakacindy <isakacindy@gmail.com>
2024-05-29 15:21:34 +0900

    middleware 중간 수정

--- BE/Server/middleware/auth.js
+++ BE/Server/middleware/auth.js
@@ -1,16 +1,27 @@
 const { User } = require('../models/User');
+const jwt = require("jsonwebtoken");
+//const secret = process.env.JWT_SECRET_KEY;
 
 let auth = async (req, res, next) => {
     try {
         let token = req.cookies.x_auth;
-        let user = await User.findByToken(token);
-        if (!user) return res.json({ isAuth: false, error: true });
+        
+        // 토큰 검증
+        jwt.verify(token, secret, async (error, decoded) => {
+            if (error) {
+                return res.json({ isAuth: false, error: true });
+            }
+            let user = await User.findOne({_id: decoded.id});
+            if (!user) {
+                return res.json({ isAuth: false, error: true });
+            }
 
-        req.token = token;
-        req.user = user;
-        next();
+            req.token = token;
+            req.user = user;
+            next();
+        });
     } catch (err) {
-        throw err;
+        return res.status(500).send(err);
     }
 };
 
