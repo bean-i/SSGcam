6d9253d8b9d379b448f04d9e3941901e6aeb7264
leebeen <leebin0603@gmail.com>
2024-06-04 01:50:20 +0900

    탐지기록 화면 ui 변경

--- FE/lib/main.dart
+++ FE/lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:logger/logger.dart';
-import 'package:audio_session/audio_session.dart';
 import 'screens/main_screen.dart';
 import 'screens/onboarding.dart';
 import 'screens/login.dart';
